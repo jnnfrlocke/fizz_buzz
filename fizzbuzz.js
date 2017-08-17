5520e35a569c77db96c75c3c856e229dc02f32f0
Jen <jnnfrlocke@gmail.com>
2017-08-17 16:13:49 -0500

    Add index.html and complete fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,16 @@
 "use strict";
 
-var integer = 0;
+var i;
+
+for (i = 0; i <= 100; i++) {
+    if (i === 0)
+        console.log(0);
+    else if (i % 3 === 0 && i % 5 === 0)
+        console.log("fizz buzz");
+    else if (i % 3 === 0)
+        console.log("fizz");
+    else if (i % 5 === 0)
+        console.log("buzz");
+    else
+        console.log(i);
+}
